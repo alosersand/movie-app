df4fc1549d19bab371daca5b37f83244546cc25f
lazyBit99 <alex.marchi99@gmail.com>
2020-04-23 17:50:01 +0200

    add ListView

--- app/src/main/java/it/alessandromarchi/movieapp/MainActivity.java
+++ app/src/main/java/it/alessandromarchi/movieapp/MainActivity.java
@@ -3,10 +3,13 @@ package it.alessandromarchi.movieapp;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView movieList;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        movieList = findViewById(R.id.movie_list);
 
 
     }
