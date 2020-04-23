0da2aaec2c4b91a98ac6a4cf5e8bcd06964cccc4
lazyBit99 <alex.marchi99@gmail.com>
2020-04-23 17:10:21 +0200

    add toolbar button

--- app/src/main/java/it/alessandromarchi/movieapp/MainActivity.java
+++ app/src/main/java/it/alessandromarchi/movieapp/MainActivity.java
@@ -1,14 +1,26 @@
 package it.alessandromarchi.movieapp;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
