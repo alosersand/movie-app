476bf67b905610d2dde5ffdacd3b4d524f26754a
lazyBit99 <alex.marchi99@gmail.com>
2020-04-24 17:43:25 +0200

    create SeenMovies activity

---
+++ app/src/main/java/it/alessandromarchi/movieapp/activities/SeenMovies.java
@@ -0,0 +1,16 @@
+package it.alessandromarchi.movieapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import it.alessandromarchi.movieapp.R;
+
+public class SeenMovies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seen_movies);
+    }
+}
