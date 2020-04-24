ee6e48872c98f1188401ae1c8c367708d358c8fe
lazyBit99 <alex.marchi99@gmail.com>
2020-04-24 17:41:40 +0200

    WIP added database query

--- app/src/main/java/it/alessandromarchi/movieapp/activities/MovieDetail.java
+++ app/src/main/java/it/alessandromarchi/movieapp/activities/MovieDetail.java
@@ -1,16 +1,23 @@
 package it.alessandromarchi.movieapp.activities;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import it.alessandromarchi.movieapp.R;
+import it.alessandromarchi.movieapp.database.MovieDB;
+import it.alessandromarchi.movieapp.database.MovieTableHelper;
 
 public class MovieDetail extends AppCompatActivity {
 
     TextView title;
+
+    SQLiteDatabase database;
+    MovieDB movieDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +26,35 @@ public class MovieDetail extends AppCompatActivity {
 
         title = findViewById(R.id.title);
 
+        movieDB = new MovieDB(this);
+        database = movieDB.getReadableDatabase();
+
         Intent intent = getIntent();
         Long id = intent.getLongExtra("movie_id", 0);
 
-        title.setText(id.toString());
+        setTitle(id.toString());
+
+        Cursor movies = database.query(
+                MovieTableHelper.TABLE_NAME,
+                new String[]{
+                        MovieTableHelper.IS_SEEN,
+                        MovieTableHelper._ID
+                },
+                MovieTableHelper._ID + " = " + id,
+                null,
+                null,
+                null,
+                null
+        );
+
+        movies.moveToNext();
+        if (movies.getCount() >= 1) {
+            title.setText("isSeen = " + movies.getInt(movies.getColumnIndex(MovieTableHelper.IS_SEEN)));
+        } else {
+            title.setText("ERRORE");
+        }
+
+        movies.close();
+        database.close();
     }
 }
