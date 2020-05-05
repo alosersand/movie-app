838e654b1d63b5812bc550a4a13368106d37f201
lazyBit99 <alex.marchi99@gmail.com>
2020-05-05 11:16:47 +0200

    WIP web services implementation

--- app/src/main/java/it/alessandromarchi/movieapp/activities/MainActivity.java
+++ app/src/main/java/it/alessandromarchi/movieapp/activities/MainActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,6 +22,8 @@ import androidx.loader.app.LoaderManager;
 import androidx.loader.content.CursorLoader;
 import androidx.loader.content.Loader;
 
+import java.util.List;
+
 import it.alessandromarchi.movieapp.R;
 import it.alessandromarchi.movieapp.adapters.MovieAdapter;
 import it.alessandromarchi.movieapp.database.MovieDB;
@@ -27,123 +31,152 @@ import it.alessandromarchi.movieapp.database.MovieProvider;
 import it.alessandromarchi.movieapp.database.MovieTableHelper;
 import it.alessandromarchi.movieapp.fragments.ConfirmDialogFragment;
 import it.alessandromarchi.movieapp.fragments.ConfirmDialogFragmentListener;
+import it.alessandromarchi.movieapp.models.Movie;
+import it.alessandromarchi.movieapp.services.WebService;
+import it.alessandromarchi.movieapp.services.iWebServer;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, ConfirmDialogFragmentListener {
 
-    private static final int LOADER_ID = 568175;
+	private static final int LOADER_ID = 568175;
 
-    SQLiteDatabase database;
-    MovieDB movieDB;
-    MovieAdapter movieAdapter;
+	SQLiteDatabase database;
+	MovieDB movieDB;
+	MovieAdapter movieAdapter;
 
-    GridView moviesGrid;
-    MenuItem action_wishlist;
+	GridView moviesGrid;
+	MenuItem actionWishlist;
+	ProgressBar progressBar;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.action_menu, menu);
+	WebService webService;
+	iWebServer webServerListener = new iWebServer() {
+		@Override
+		public void onMoviesFetched(boolean success, List<Movie> movies, int errorCode, String errorMessage) {
+			if (success) {
+				movieAdapter.setMovie(movies);
+				movieAdapter.notifyDataSetChanged();
+				progressBar.setVisibility(View.GONE);
+				moviesGrid.setVisibility(View.VISIBLE);
 
-        action_wishlist = menu.getItem(0);
-        action_wishlist.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                Intent wishlist = new Intent(MainActivity.this, Wishlist.class);
-                startActivity(wishlist);
+				Log.d("♦", "onMoviesFetched: " + movies);
+			} else {
+				progressBar.setVisibility(View.VISIBLE);
+				moviesGrid.setVisibility(View.GONE);
 
-                return true;
-            }
-        });
+				Log.d("♦", "onMoviesFetched: " + errorMessage);
+			}
+		}
+	};
 
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.action_menu, menu);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+		actionWishlist = menu.getItem(0);
+		actionWishlist.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+			@Override
+			public boolean onMenuItemClick(MenuItem item) {
+				Intent wishlist = new Intent(MainActivity.this, Wishlist.class);
+				startActivity(wishlist);
 
-        movieDB = new MovieDB(this);
-        movieAdapter = new MovieAdapter(this, null);
+				return true;
+			}
+		});
 
-        moviesGrid = findViewById(R.id.movies_grid);
-        moviesGrid.setAdapter(movieAdapter);
-        moviesGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent movieDetail = new Intent(MainActivity.this, MovieDetail.class);
-                movieDetail.putExtra("movie_id", id);
-                startActivity(movieDetail);
-            }
-        });
-        moviesGrid.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                ConfirmDialogFragment dialogFragment;
+		return true;
+	}
 
-                database = movieDB.getReadableDatabase();
-                Cursor titles = database.query(
-                        MovieTableHelper.TABLE_NAME,
-                        new String[]{
-                                MovieTableHelper.TITLE,
-                                MovieTableHelper._ID
-                        },
-                        MovieTableHelper._ID + " = " + id,
-                        null,
-                        null,
-                        null,
-                        null
-                );
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-                titles.moveToNext();
-                if (titles.getCount() >= 1) {
-                    dialogFragment = new ConfirmDialogFragment(getString(R.string.dialog_confirm, titles.getString(titles.getColumnIndex(MovieTableHelper.TITLE))), id);
-                } else {
-                    dialogFragment = new ConfirmDialogFragment(getString(R.string.dialog_error_confirm), id);
-                }
-                titles.close();
+		webService = WebService.getInstance();
 
-                dialogFragment.show(fragmentManager, ConfirmDialogFragment.class.getName());
+		movieDB = new MovieDB(this);
+		movieAdapter = new MovieAdapter(this, null);
 
-                return true;
-            }
-        });
+		progressBar = findViewById(R.id.progressBar);
 
-        //TODO aggiornare con metodo non deprecato
-        getSupportLoaderManager().initLoader(LOADER_ID, null, this);
-    }
+		moviesGrid = findViewById(R.id.movies_grid);
+		moviesGrid.setAdapter(movieAdapter);
+		moviesGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Intent movieDetail = new Intent(MainActivity.this, MovieDetail.class);
+				movieDetail.putExtra("movie_id", id);
+				startActivity(movieDetail);
+			}
+		});
+		moviesGrid.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+				FragmentManager fragmentManager = getSupportFragmentManager();
+				ConfirmDialogFragment dialogFragment;
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+				database = movieDB.getReadableDatabase();
+				Cursor titles = database.query(
+								MovieTableHelper.TABLE_NAME,
+								new String[]{
+												MovieTableHelper.TITLE,
+												MovieTableHelper._ID
+								},
+								MovieTableHelper._ID + " = " + id,
+								null,
+								null,
+								null,
+								null
+				);
 
-        movieAdapter.notifyDataSetChanged();
-    }
+				titles.moveToNext();
+				if (titles.getCount() >= 1) {
+					dialogFragment = new ConfirmDialogFragment(getString(R.string.dialog_confirm, titles.getString(titles.getColumnIndex(MovieTableHelper.TITLE))), id);
+				} else {
+					dialogFragment = new ConfirmDialogFragment(getString(R.string.dialog_error_confirm), id);
+				}
+				titles.close();
 
-    @NonNull
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
-        return new CursorLoader(this, MovieProvider.MOVIES_URI, null, null, null, null);
-    }
+				dialogFragment.show(fragmentManager, ConfirmDialogFragment.class.getName());
 
-    @Override
-    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
-        movieAdapter.changeCursor(data);
-    }
+				return true;
+			}
+		});
 
-    @Override
-    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
-        movieAdapter.changeCursor(null);
-    }
+		//TODO aggiornare con metodo non deprecato
+		getSupportLoaderManager().initLoader(LOADER_ID, null, this);
+	}
 
-    @Override
-    public void onPositivePressed(long movieID) {
-        Toast.makeText(this, R.string.wishlist_add, Toast.LENGTH_SHORT).show();
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
 
-    @Override
-    public void onNegativePressed() {
+		webService.getMovies(webServerListener);
+		movieAdapter.notifyDataSetChanged();
+	}
 
-    }
+	@NonNull
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+		return new CursorLoader(this, MovieProvider.MOVIES_URI, null, null, null, null);
+	}
+
+	@Override
+	public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+		movieAdapter.changeCursor(data);
+	}
+
+	@Override
+	public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+		movieAdapter.changeCursor(null);
+	}
+
+	@Override
+	public void onPositivePressed(long movieID) {
+		Toast.makeText(this, R.string.wishlist_add, Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onNegativePressed() {
+
+	}
 }
