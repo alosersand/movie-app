58991f71f59594456d36612b7e2cab7d4d9d48f4
lazyBit99 <alex.marchi99@gmail.com>
2020-05-29 15:23:18 +0200

    FORMAT code

--- app/src/main/java/it/alessandromarchi/movieapp/fragments/ConfirmDialogFragment.java
+++ app/src/main/java/it/alessandromarchi/movieapp/fragments/ConfirmDialogFragment.java
@@ -15,63 +15,63 @@ import it.alessandromarchi.movieapp.R;
 
 public class ConfirmDialogFragment extends DialogFragment {
 
-    private String title;
-    private String message;
-    private long movieID;
+	private String title;
+	private String message;
+	private long movieID;
 
-    private ConfirmDialogFragmentListener listener;
+	private ConfirmDialogFragmentListener listener;
 
-    public ConfirmDialogFragment(String title, String message, long movieID) {
-        this.title = title;
-        this.message = message;
-        this.movieID = movieID;
-    }
+	public ConfirmDialogFragment(String title, String message, long movieID) {
+		this.title = title;
+		this.message = message;
+		this.movieID = movieID;
+	}
 
-    public ConfirmDialogFragment(String message, long movieID) {
-        this.message = message;
-        this.movieID = movieID;
-    }
+	public ConfirmDialogFragment(String message, long movieID) {
+		this.message = message;
+		this.movieID = movieID;
+	}
 
-    @Override
-    public void onAttach(@NonNull Activity activity) {
-        if (activity instanceof ConfirmDialogFragmentListener) {
-            listener = (ConfirmDialogFragmentListener) activity;
-        } else {
-            listener = null;
-        }
+	@Override
+	public void onAttach(@NonNull Activity activity) {
+		if (activity instanceof ConfirmDialogFragmentListener) {
+			listener = (ConfirmDialogFragmentListener) activity;
+		} else {
+			listener = null;
+		}
 
-        super.onAttach(activity);
-    }
+		super.onAttach(activity);
+	}
 
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+	@NonNull
+	@Override
+	public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+		AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
 
-			String positiveButton;
+		String positiveButton;
 
-        alertDialog.setTitle(title);
-			alertDialog.setMessage(Html.fromHtml(message));
+		alertDialog.setTitle(title);
+		alertDialog.setMessage(Html.fromHtml(message));
 
-			if (title.equals(getString(R.string.add_title))) {
-				positiveButton = getString(R.string.add);
-			} else {
-				positiveButton = getString(R.string.remove);
+		if (title.equals(getString(R.string.add_title))) {
+			positiveButton = getString(R.string.add);
+		} else {
+			positiveButton = getString(R.string.remove);
+		}
+
+		alertDialog.setPositiveButton(positiveButton, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				listener.onPositivePressed(movieID);
 			}
+		});
+		alertDialog.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				listener.onNegativePressed();
+			}
+		});
 
-			alertDialog.setPositiveButton(positiveButton, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                listener.onPositivePressed(movieID);
-            }
-        });
-        alertDialog.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                listener.onNegativePressed();
-            }
-        });
-
-        return alertDialog.create();
-    }
+		return alertDialog.create();
+	}
 }
