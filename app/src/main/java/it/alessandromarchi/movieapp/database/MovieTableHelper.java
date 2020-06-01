6ac861ef2c2156e0b338e4c598146489f823afe8
lazyBit99 <alex.marchi99@gmail.com>
2020-06-01 20:05:45 +0200

    ADD rating value

--- app/src/main/java/it/alessandromarchi/movieapp/database/MovieTableHelper.java
+++ app/src/main/java/it/alessandromarchi/movieapp/database/MovieTableHelper.java
@@ -4,18 +4,20 @@ import android.provider.BaseColumns;
 
 public class MovieTableHelper implements BaseColumns {
 
-    public static final String TABLE_NAME = "movies";
-    public static final String TITLE = "title";
-    public static final String DESCRIPTION = "description";
-    public static final String IMAGE_PATH = "image_path";
+	public static final String TABLE_NAME = "movies";
+	public static final String TITLE = "title";
+	public static final String DESCRIPTION = "description";
+	public static final String IMAGE_PATH = "image_path";
 	public static final String BACKGROUND_PATH = "background_path";
-    public static final String IS_WISHLIST = "is_wishlist";
+	public static final String IS_WISHLIST = "is_wishlist";
+	public static final String RATING = "rating";
 
-    public static final String CREATE = "CREATE TABLE " + TABLE_NAME + " (" +
-            _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            TITLE + " TEXT, " +
-            DESCRIPTION + " TEXT, " +
-            IMAGE_PATH + " TEXT, " +
+	public static final String CREATE = "CREATE TABLE " + TABLE_NAME + " (" +
+			_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+			TITLE + " TEXT, " +
+			DESCRIPTION + " TEXT, " +
+			IMAGE_PATH + " TEXT, " +
 			BACKGROUND_PATH + " TEXT, " +
-            IS_WISHLIST + " INTEGER DEFAULT 0);";
+			RATING + " FLOAT, " +
+			IS_WISHLIST + " INTEGER DEFAULT 0);";
 }
